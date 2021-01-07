048c77211ab305a5e95f89bfd3406f3d6099e03b
anshumandikshit <anshuman.dikshit@gmail.com>
2021-01-08 01:15:08 +0530

    add AzureFunctionSqlDatabase project in source control

---
+++ AzureFunctionample/dbo/Tables/FeedBackForm.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[FeedBackForm] (
+    [Id]              BIGINT         IDENTITY (1, 1) NOT NULL,
+    [FirstName]       NVARCHAR (MAX) NULL,
+    [LastName]        NVARCHAR (MAX) NULL,
+    [EmailAddress]    NVARCHAR (MAX) NULL,
+    [FeedbackMessage] NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
